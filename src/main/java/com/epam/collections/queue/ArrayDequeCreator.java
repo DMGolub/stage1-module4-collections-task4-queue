b6682a9aa43c21da218bea2daa64381565d328ae
Dmitry Golub <dmgolub@mail.ru>
2023-01-04 01:02:17 +0400

    Temp.

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -5,6 +5,29 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> commonQueue = new ArrayDeque<>();
+        for (int i = 0; i < 2; i++) {
+            commonQueue.add(firstQueue.poll());
+        }
+        for (int j = 0; j < 2; j++) {
+            commonQueue.add(secondQueue.poll());
+        }
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            firstQueue.add(commonQueue.poll());
+            for (int i = 0; i < 2; i++) {
+                if (!firstQueue.isEmpty()) {
+                    commonQueue.add(firstQueue.poll());
+                }
+            }
+            secondQueue.add(commonQueue.poll());
+            for (int j = 0; j < 2; j++) {
+                if (!secondQueue.isEmpty()) {
+                    commonQueue.add(secondQueue.poll());
+                }
+            }
+        }
+        return commonQueue;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,24 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
+
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishes = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishes.add(i);
+        }
+        List<Integer> eatenDishes = new ArrayList<>();
+        int index = everyDishNumberToEat - 1;
+        while (!dishes.isEmpty()) {
+            eatenDishes.add(dishes.remove(index));
+            if (!dishes.isEmpty()) {
+                index = (index + everyDishNumberToEat - 1) % dishes.size();
+            }
+        }
+        return eatenDishes;
     }
 }
